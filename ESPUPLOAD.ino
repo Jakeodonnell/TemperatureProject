3b40d27cbf4b9db35f9b1d5c0fd0ad9abd8bb45a
Unknown <imjakeeyy@gmail.com>
2019-01-04 11:56:07 +0100

    ESP8266 to Firebase
    ESP8266 connection to wi-fi and uppload value to firebase database

---
+++ ESPUPLOAD.ino
@@ -0,0 +1,104 @@
+//
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// FirebaseDemo_ESP8266 is a sample that demo the different functions
+// of the FirebaseArduino API.
+
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "test-de47e.firebaseio.com"
+#define FIREBASE_AUTH "G4RM5PNSqf0R89dMvoRf7HJLLZx538dvZ97jYp4r"
+#define WIFI_SSID "AstaJake"
+#define WIFI_PASSWORD "vgyvgy789"
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 5
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device address
+DeviceAddress insideThermometer;
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+  //firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  //DallasTemp
+   Serial.print("Locating devices...");
+  sensors.begin();
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: "); 
+  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
+  Serial.print("Device 0 Address: ");
+  Serial.println();
+  sensors.setResolution(insideThermometer, 9);
+ 
+  Serial.print("Device 0 Resolution: ");
+  Serial.print(sensors.getResolution(insideThermometer), DEC); 
+  Serial.println();
+}
+
+int printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  Serial.print("Temp C: ");
+  Serial.print(tempC);
+  int TempC = sensors.getTempC(deviceAddress);
+  return TempC;
+}
+
+
+void loop() {
+  //SensorData
+  sensors.requestTemperatures();
+  Serial.println("DONE");
+  float tempToSend =   printTemperature(insideThermometer);
+  //Firebase upplload
+  Firebase.setInt("logs", tempToSend);
+  if(Firebase.failed())
+  {
+    Serial.println(Firebase.error());
+  }
+  Serial.println("Uploaded");
+  delay(3000);
+}
